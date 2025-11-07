ffdc66c7eb5e386aed1d77056e3a9f04c8577375
EvanCh08 <evandarren04@gmail.com>
2025-11-07 12:31:44 +0800

    ID changed from number to string

--- types/common.ts
+++ types/common.ts
@@ -1,8 +1,2 @@
-export type ID = number;
-
-export interface Timestamps {
-  created_at?: string; // ISO
-  updated_at?: string; // ISO
-}
-
+export type ID = string;
 export type Decimal = number; // store/transport as number in API
