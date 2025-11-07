210ebd91653ed651eb8c9332e9cb759682a58067
EvanCh08 <evandarren04@gmail.com>
2025-11-07 12:29:38 +0800

    fixed ID from number to string

--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -23,7 +23,7 @@ export default function TestPage() {
         <button
           onClick={() =>
             login({
-              user_id: 1,
+              user_id: "1",
               email: "evan@example.com",
               display_name: "Evan",
               role: "admin",
