81e3d4c86a876cbb86eaa57f055e0ed5e4f8221e
EvanCh08 <evandarren04@gmail.com>
2025-11-07 13:06:38 +0800

    set up router, landing page, and tailwind colors

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,60 +1,37 @@
 'use client'
 
+
 import type { Stall } from "../../types/stall"
+import Link from "next/link";
 
 import { useState, useEffect } from "react"
 import { Button } from "@/components/ui/button"
 import { Search } from "lucide-react"
 
+
 export default function Home() {
-  const [stalls, setStalls] = useState<Stall[]>([])
-  const [loading, setLoading] = useState<boolean>(true)
 
-  useEffect(() => {
-    async function fetchStalls() {
-      try {
-        const response = await fetch("http://localhost:4000/stalls")
-        if (!response.ok) {
-          throw new Error(`HTTP ${response.status} ${response.statusText}`)
-        }
 
-        const data: Stall[] = await response.json()
-        setStalls(data)
-      } catch (err) {
-        console.error("Failed to fetch stalls:", err)
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    fetchStalls()
-  }, [])
-
-  if (loading) return <p>Loading...</p>
-
-  return (
-    <main className="p-2">
-      <div>
-        <h1>Stalls:</h1>
-        {stalls.length > 0 ? (
-          <ul>
-            {/* ✅ Type-safe access to stall.id and stall.name */}
-            {stalls.map((stall) => (
-              <li key={stall.stall_id}>{stall.name}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No stalls found.</p>
-        )}
-      </div>
-
-      <div className="flex flex-col w-[100px] space-y-3 my-4">
-        <Button>Default</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="destructive">Destructive</Button>
-        <Button variant="outline">Outline</Button>
-        <Search className="w-4 h-4 text-muted-foreground" />
-      </div>
-    </main>
-  )
+ return (
+   <main className="min-h-screen flex items-center justify-center">
+     <div className="w-[400px] h-[300px] items-center flex flex-col justify-center border-1 rounded-md shadow-lg">
+       <div className="my-2 flex justify-center items-center flex-col w-full">
+         <div>
+           <h1 className="font-semibold text-2xl">Choose a Module</h1>
+         </div>
+         <div className="flex flex-col w-[250px] space-y-5 my-5">
+           <Button className="bg-primary1 h-11 rounded-md">
+            <Link href="/admin">Admin</Link>
+           </Button>
+           <Button className="bg-primary1 h-11 rounded-md">
+            <Link href="/runner">Runner</Link>
+           </Button>
+           <Button className="bg-primary1 h-11 rounded-md">
+            <Link href="/ordering">Ordering</Link>
+           </Button>
+         </div>
+       </div>
+     </div>
+   </main>
+ )
 }
