ea33f363523d2501351a815937ae0cbd3776c0ef
RaphaelYeo <Raphael_YEO@htx.gov.sg>
2026-03-26 12:28:15 +0800

    add migration for volunteer_name column

---
+++ backend/migrations/0006_add_volunteername_column.sql
@@ -0,0 +1,11 @@
+-- Add is_active column and remove qr_code column from table
+BEGIN;
+
+-- Add volunteer_name column to order
+ALTER TABLE "order" ADD COLUMN volunteer_name VARCHAR NOT NULL;
+
+-- Add volunteer_name column to custom_order_item
+ALTER TABLE custom_order_item ADD COLUMN volunteer_name VARCHAR NOT NULL;
+
+
+COMMIT;
